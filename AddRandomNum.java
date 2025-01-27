8cfd697a91426065ee3fc5a5d6be04fd686850df
sanjana1035 <njsanjana10@gmail.com>
2025-01-28 00:52:32 +0530

    Add files via upload

---
+++ AddRandomNum.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class AddRandomNum{
+public static void main(String[] args){
+Scanner sc = new Scanner(System.in);
+int num1 = sc.nextInt();
+int num2 = sc.nextInt();
+int sum = num1+num2;
+System.out.println("The 1st Number is :" +num1);
+System.out.println("The 2nd Number is :" +num2);
+System.out.println("The Sum is:" +sum);
+
+}
+
+}
